0585567a195ec0dafe70266d5b4e73afbbaefdc6
wendellmccaulay@hotmail.com <wendellmccaulay@hotmail.com>
2018-02-11 18:36:49 -0300

    Displaying comments

--- src/app/mock-post.ts
+++ src/app/mock-post.ts
@@ -15,8 +15,8 @@ export const posts: Post[] = [
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         `,
         comments: [
-            'So cool, what a great idea you had when you decided to create this blog',
-            'I think your writing style is very good',
+            'So cool, what a great idea you had when you decided to create this blog.',
+            'I think your writing style is very good.',
             'Ow, I think you are the only person that writes in Latin nowadays!'
         ]
     },
